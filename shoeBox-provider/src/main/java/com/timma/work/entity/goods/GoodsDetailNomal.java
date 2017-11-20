b42b14949edbd994ca511f7254e29e9bf908d67d
wangzhichao <hzwangzhichao1@corp.netease.com>
2017-11-20 21:11:01 +0800

    基础工程搭建

---
+++ shoeBox-provider/src/main/java/com/timma/work/entity/goods/GoodsDetailNomal.java
@@ -0,0 +1,13 @@
+package com.timma.work.entity.goods;
+
+import java.io.Serializable;
+
+/**
+ * @Description: 商品模型
+ *
+ * @Author: hzwangzhichao1@corp.netease.com
+ * @Date: 2017/11/20
+ */
+public class GoodsDetailNomal implements Serializable{
+
+}
